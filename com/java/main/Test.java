c30746ecec85e861f81650f16209faf6e649ef01
Tushar028 <tushartanan@gmail.com>
2025-02-04 14:38:07 +0530

    first commit from feature1 - used stream to process over list
    second commit - one statement added
    
    third commit from feature1 - one more statement added

--- com/java/main/Test.java
+++ com/java/main/Test.java
@@ -9,6 +9,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Integer> lst = Arrays.asList(1,3,5,6,7,8,10);
+		System.out.println("printing even values");
+		lst.stream().filter(i -> i%2==0).forEach(System.out::println);
+		System.out.println("so these were the values you wanted");
+		
 	}
 
 }
