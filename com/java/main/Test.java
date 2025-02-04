f0cf7f13b0f0c215514859cc8b519163d7a9119c
Tushar028 <tushartanan@gmail.com>
2025-02-04 14:34:45 +0530

    first commit - added a list of integer

---
+++ com/java/main/Test.java
@@ -0,0 +1,14 @@
+package com.java.main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> lst = Arrays.asList(1,3,5,6,7,8,10);
+	}
+
+}
